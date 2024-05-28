c069a515ac740f5a5828f6d2ad52e33ed94ea04d
dungphan <dung.phan@braincreators.com>
2024-05-28 09:33:47 +0200

    feat: create basic template of days in a week

---
+++ src/components/DayBlock.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const { day } = defineProps<{
+  day: string
+}>()
+</script>
+<template>
+  <div class="flex justify-between pb-3 border-b-2 border-black">
+      <p class="text-[21px]">
+        <b>27.05</b>
+      </p>
+      <p class="text-[21px] text-gray-300">
+        {{ day }}
+      </p>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import CurrentDateMonth from '../components/CurrentDateMonth.vue'
+import DayBlock from '../components/DayBlock.vue'
+const daysInWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 </script>
 
 <template>
-  <header class="p-6">
+  <header class="p-6 pb-12">
     <CurrentDateMonth />
   </header>
+  <section class="p-6 flex flex-wrap gap-7">
+    <div v-for="day in daysInWeek" :key="day" class="flex-1">
+      <DayBlock :day="day" />
+    </div>
+  </section>
 </template>
